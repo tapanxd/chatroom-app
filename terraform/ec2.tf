1e6e6e79a4f7efd77dca55f2a9b1d8e6c3ad941a
Tapan Panchal <tp792175@dal.ca>
2025-04-19 01:11:38 -0300

    update ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,3 +1,19 @@
+# Get latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 # EC2 Instance
 resource "aws_instance" "web_server" {
   ami                    = data.aws_ami.amazon_linux_2.id
